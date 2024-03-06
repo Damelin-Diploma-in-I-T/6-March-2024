8b2d6b6c6a21af284f8713ce208c9e5901d478ad
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2024-03-06 11:48:12 +0200

    Add files via upload

---
+++ March_6_2024/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Integer
+        'Din Grade As Integar'
+
+        If (x = 100) Then
+            MsgBox("Svholler")
+
+        ElseIf (x = 0) Then
+            MsgBox("Bachualar")
+
+        End If
+
+
+    End Sub
+End Class
